faa80e627c0a4b6be17cf28c71e683a4630f55dc
Kehan Wang <sheben404@gmail.com>
2023-04-04 14:37:50 +0800

    feat: complete a general configuration parser

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,8 +2,11 @@ import { createServer as createViteDevServer } from 'vite';
 import { pluginIndexHtml } from './plugin-repress/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
+import { resolveConfig } from './config';
 
 export async function createDevServer(root = process.cwd()) {
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
