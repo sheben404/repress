81cbb74f2cd85f8003346449eff2717d5d12ec71
Kehan Wang <sheben404@gmail.com>
2023-04-06 16:30:03 +0800

    feat: add unocss

--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -1,8 +1,10 @@
+import 'uno.css';
 import { Content } from '@runtime';
+
 export function Layout() {
   return (
     <div>
-      <h1>Common Content</h1>
+      <h1 p="2">Common Content</h1>
       <h1>Doc Content</h1>
       <Content />
     </div>
