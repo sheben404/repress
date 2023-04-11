ee4711e3631fb231fa14949320e3ff7f2c755915
Kehan Wang <sheben404@gmail.com>
2023-04-11 16:34:28 +0800

    feat: update test case

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -3,9 +3,11 @@ import { test, expect } from '@playwright/test';
 const siteUrl = 'http://localhost:5173';
 test('Verify that the page renders properly', async ({ page }) => {
   await page.goto(siteUrl);
+  // 所有网络请求处理完成之后
+  await page.waitForLoadState('networkidle');
   const res = await page.evaluate(async () => {
     const pageContent = document.body.innerText;
-    return pageContent.includes('This');
+    return pageContent.includes('Hello');
   });
   expect(res).toBe(true);
 });
