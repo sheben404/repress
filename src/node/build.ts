41358545086c39d5480df96edfb573fe148469d8
Kehan Wang <sheben404@gmail.com>
2023-04-11 13:38:15 +0800

    feat: constructing a virtual module and packaging Repress component code

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,5 +1,9 @@
 import { InlineConfig, build as viteBuild } from 'vite';
-import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants';
+import {
+  CLIENT_ENTRY_PATH,
+  MASK_SPLITTER,
+  SERVER_ENTRY_PATH
+} from './constants';
 import type { RollupOutput } from 'rollup';
 import path, { dirname, join } from 'path';
 import fs from 'fs-extra';
@@ -7,6 +11,7 @@ import ora from 'ora';
 import { SiteConfig } from 'shared/types';
 import { createVitePlugins } from './vitePlugins';
 import { Route } from './plugin-routes';
+import { RenderResult } from 'runtime/ssr-entry';
 
 export async function bundle(root: string, config: SiteConfig) {
   const resolveViteConfig = async (
@@ -48,8 +53,72 @@ export async function bundle(root: string, config: SiteConfig) {
   }
 }
 
+async function buildRepress(
+  root: string,
+  repressPathToMap: Record<string, string>
+) {
+  // { Aside: 'xxx' }
+  // 内容
+  // import { Aside } from 'xxx'
+  // window.REPRESS = { Aside }
+  // window.REPRESS_PROPS = JSON.parse(
+  // document.getElementById('repress-props').textContent
+  // );
+  const repressInjectCode = `
+    ${Object.entries(repressPathToMap)
+      .map(
+        ([repressName, repressPath]) =>
+          `import { ${repressName} } from '${repressPath}'`
+      )
+      .join('')}
+window.REPRESS = { ${Object.keys(repressPathToMap).join(', ')} };
+window.REPRESS_PROPS = JSON.parse(
+  document.getElementById('repress-props').textContent
+);
+  `;
+  const injectId = 'repress:inject';
+  return viteBuild({
+    mode: 'production',
+    build: {
+      outDir: path.join(root, '.temp'),
+      rollupOptions: {
+        input: injectId
+      }
+    },
+    plugins: [
+      {
+        name: 'repress:inject',
+        enforce: 'post',
+        resolveId(id) {
+          if (id.includes(MASK_SPLITTER)) {
+            const [originId, importer] = id.split(MASK_SPLITTER);
+            return this.resolve(originId, importer, { skipSelf: true });
+          }
+
+          if (id === injectId) {
+            return id;
+          }
+        },
+        load(id) {
+          if (id === injectId) {
+            return repressInjectCode;
+          }
+        },
+        // 对于 Repress Bundle，我们只需要 JS 即可，其它资源文件可以删除
+        generateBundle(_, bundle) {
+          for (const name in bundle) {
+            if (bundle[name].type === 'asset') {
+              delete bundle[name];
+            }
+          }
+        }
+      }
+    ]
+  });
+}
+
 export async function renderPage(
-  render: (routePath: string) => string,
+  render: (routePath: string) => RenderResult,
   routes: Route[],
   root: string,
   clientBundle: RollupOutput
@@ -61,7 +130,8 @@ export async function renderPage(
   return Promise.all(
     routes.map(async (route) => {
       const routePath = route.path;
-      const appHtml = await render(routePath);
+      const { appHtml, repressToPathMap, propsData } = await render(routePath);
+      buildRepress(root, repressToPathMap);
       const html = `
 <!DOCTYPE html>
 <html>
--- src/runtime/ssr-entry.tsx
+++ src/runtime/ssr-entry.tsx
@@ -3,13 +3,19 @@ import { App, initPageData } from './app';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 
+export interface RenderResult {
+  appHtml: string;
+  propsData: unknown[];
+  repressToPathMap: Record<string, string>;
+}
+
 // For ssr component render
 export async function render(pagePath: string) {
   const pageData = await initPageData(pagePath);
   const { clearRepressData, data } = await import('./jsx-runtime');
   const { repressProps, repressToPathMap } = data;
   clearRepressData();
-  return renderToString(
+  const appHtml = renderToString(
     <DataContext.Provider value={pageData}>
       <StaticRouter location={pagePath}>
         <App />
